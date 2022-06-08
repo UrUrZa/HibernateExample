a2027efcba814b18b35edc398a644d95d60fa8f9
UrUrZa <IshvarIndrovich@mail.ru>
2022-06-08 17:37:59 +0400

    working state

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,36 @@
+package person;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Persister;
+
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 128)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Person {id = " + String.valueOf(id) + ", name = '" + name + "'}";
+    }
+}
